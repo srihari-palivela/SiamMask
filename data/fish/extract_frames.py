15f91147bea061bdd1f818ab86ee26c0f2523841
Srihari Palivela <hari@gytworkz.com>
2020-12-20 11:35:23 +0530

    frames

--- data/fish/extract_frames.py
+++ data/fish/extract_frames.py
@@ -1,5 +1,20 @@
 import cv2
-vidcap = cv2.VideoCapture('00101.MTS')
+import sys
+import shutil
+import os
+
+try:
+    filename = sys.argv[1]
+    fname, ext = filename.split(".")
+except:
+    exit(1)
+
+dirpath = os.path.isdir(fname)
+if os.path.exists(dirpath) and os.path.isdir(dirpath):
+	shutil.rmtree(dirpath)
+        os.makedirs(op.path.dir_name(dirpath))
+        
+vidcap = cv2.VideoCapture(filename)
 fps = vidcap.get(cv2.CAP_PROP_FPS)
 print("FPS : ",vidcap.get(cv2.CAP_PROP_FPS))
 # timestamps = [vidcap.get(cv2.CAP_PROP_POS_MSEC)]
@@ -9,7 +24,8 @@ count = 0
 frame_step = 1/fps
 while success:
   frame_timestamp = "{sec}.{frame_number}".format(sec=str(int(count/fps)).zfill(5), frame_number= str(int(count%fps)).zfill(5))
-  cv2.imwrite("frames/%s.jpg" % str(frame_timestamp), image)     # save frame as JPEG file
+  cv2.imwrite("{fname}/%s.jpg" % str(frame_timestamp), image)     # save frame as JPEG file
   success,image = vidcap.read()
   # print('Read a new frame: ', success)
   count += 1
+
